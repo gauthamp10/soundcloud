a63b37db5d281403f3d469a46dbb9420be66227f
Gautham Prakash <gauthamp10@gmail.com>
2019-07-04 15:16:10 +0530

    added correct source

--- engine.py
+++ engine.py
@@ -8,7 +8,7 @@ def get_track(url):
         track_title=str(data['title'])   
         data=requests.get("http://api.soundcloud.com/i1/tracks/"+track_id+"/streams?client_id=FweeGBOOEOYJWLJN3oEyToGLKhmSz0I7").json()
         mp3=data['http_mp3_128_url']
-        return mp3
+        return str(mp3)
         
 
     except Exception as e:
--- main.py
+++ main.py
@@ -7,7 +7,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    down_link={'downlink':'tumblr'}
     return render_template('index.html',link="#")
 
 @app.route("/",methods=['GET','POST'])
@@ -16,17 +15,9 @@ def home_post():
     down_link=get_track(url)
     return render_template('index.html',link=down_link)
 
-@app.route("/soundcloud.pw/<artist>/<song>/")
-def quick(artist,song):
-    part= artist+'/'+song
-    url='https://soundcloud.com/'+part
-    print(url)
-    down_link=get_track(url)
-    return render_template('quick_down.html',link=down_link)
-    
         
 
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host="0.0.0.0",port=8080, debug=True)
