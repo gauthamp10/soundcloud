e30338eab3d40eeba2a5f65b2e3849448897a738
Gautham Prakash <gauthamp10@gmail.com>
2020-10-01 14:59:43 +0530

    Update engine.py

--- engine.py
+++ engine.py
@@ -4,14 +4,10 @@ import json
 def get_track(url):
     try:
         data=requests.get("https://api.soundcloud.com/resolve.json?url="+url+"&client_id=a3dd183a357fcff9a6943c0d65664087").json()
-        track_id=str(data['id'])
-        track_title=str(data['title'])   
+        track_id=str(data['id']) 
         data=requests.get("http://api.soundcloud.com/i1/tracks/"+track_id+"/streams?client_id=a3dd183a357fcff9a6943c0d65664087").json()
         mp3=data['http_mp3_128_url']
         return str(mp3)
-        
-
     except Exception as e:
         print(str(e))
         return("Error!")
-
